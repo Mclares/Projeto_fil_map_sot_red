f0948a1419f9e68a6eee8d15c9a9e0bc379f339f
Marcelo Clares <msc.09.wylde@gmail.com>
2020-06-05 19:16:27 -0300

    Projeto criado fil_sor_map_red

---
+++ src/application/Program_Employees.java
@@ -0,0 +1,66 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import entities.Employees;
+
+public class Program_Employees {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter full file path: ");
+		String path = sc.nextLine();
+		System.out.print("Enter salary: ");
+		double salaryParameter = sc.nextDouble();
+				
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
+			List<Employees> list = new ArrayList<>(); 
+			
+			String line = br.readLine();
+			while(line != null) {
+				
+				String[] fields = line.split(",");
+				list.add(new Employees(fields[0], fields[1], Double.parseDouble(fields[2])));
+				line = br.readLine();
+				
+			}
+			
+			List<String> emails = list.stream()
+					.filter(e -> e.getSalary() > salaryParameter)
+					.map(e -> e.getEmail())
+					.sorted()
+					.collect(Collectors.toList());
+			
+			System.out.println();
+			System.out.println("Email of people whose salary is more than " + String.format("%.2f", salaryParameter) + ":");
+			emails.forEach(System.out::println);
+			
+			Double totalSalary = list.stream()
+					.filter(e -> e.getName().startsWith("M"))
+					.map(e -> e.getSalary())
+					.reduce(0.0, (x, y) -> x + y);
+			
+			System.out.println();
+			System.out.println("Sum of salary of people whose name starts with 'M': " + String.format("%.2f", totalSalary));
+			
+		}
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		sc.close();
+	}
+
+}
